014d50a04b8d698f985781c1ddd12833d9921fd5
Kevin Barabash <kevinb@khanacademy.org>
2016-11-02 10:11:55 -0400

    add fraction layout

--- src/ir.js
+++ src/ir.js
@@ -1,13 +1,13 @@
 // @flow
 
-type node = HBox | VBox | Char | Rule | Kern | Penalty | Special;
+type Node = HBox | VBox | Char | Rule | Kern | Glue | Penalty | Special;
 
 type Box = {
     type: 'Box',
     width?: number,  // if undefined, contents of box define its width
     height: number,
     depth: number,
-    content: node[],
+    content: Node[],
     shift: number,
 }
 
@@ -54,12 +54,12 @@ type Special = {
     depth: number,
 }
 
-type HList = node[];
-type VList = node[];
+type HList = Node[];
+type VList = Node[];
 
 const content: HList = [];
 
-const createChar = (font: FontId, char: string) => {
+const makeChar = (font: FontId, char: string) => {
     return {
         type: 'Char',
         font: font,
@@ -67,9 +67,9 @@ const createChar = (font: FontId, char: string) => {
     }
 }
 
-const sansSerifChar = createChar.bind(null, 'sans-serif');
+const sansSerifChar = makeChar.bind(null, 'sans-serif');
 
-const createKern = (amount: number) => {
+const makeKern = (amount: number) => {
     return {
         type: 'Kern',
         amount: amount,
@@ -77,13 +77,13 @@ const createKern = (amount: number) => {
 }
 
 content.push(sansSerifChar('1'))
-content.push(createKern(0.2))   // TODO(kevinb) get real kern amounts
+content.push(makeKern(0.2))   // TODO(kevinb) get real kern amounts
 content.push(sansSerifChar('+'))
-content.push(createKern(0.2))
+content.push(makeKern(0.2))
 content.push(sansSerifChar('2'))
-content.push(createKern(0.2))
+content.push(makeKern(0.2))
 content.push(sansSerifChar('='))
-content.push(createKern(0.2))
+content.push(makeKern(0.2))
 content.push(sansSerifChar('3'))
 
 type GlyphMetrics = [
@@ -117,7 +117,7 @@ const charDepth = (node: Char) => {
     return metrics[1]
 }
 
-const height = (node) => {
+const height = (node: Node) => {
     switch (node.type) {
         case 'Box': return node.height
         case 'Rule': return node.height
@@ -126,7 +126,7 @@ const height = (node) => {
     }
 }
 
-const depth = (node) => {
+const depth = (node: Node) => {
     switch (node.type) {
         case 'Box': return node.depth
         case 'Rule': return node.depth
@@ -138,15 +138,87 @@ const depth = (node) => {
 const hlistHeight = (hlist: HList) => Math.max(...hlist.map(height))
 const hlistDepth = (hlist: HList) => Math.max(...hlist.map(depth))
 
-const simpleRun: HBox = {
-    type: 'Box',
-    kind: 'HBox',
-    height: hlistHeight(content),
-    depth: hlistDepth(content),
-    content: content,
-    shift: 0,
+const makeHBox = (content: HList, shift = 0) => {
+    return {
+        type: 'Box',
+        kind: 'HBox',
+        height: hlistHeight(content),
+        depth: hlistDepth(content),
+        content: content,
+        shift: shift,
+    }
 }
+
+const simpleRun: HBox = makeHBox(content)
 
 console.log(simpleRun)
 
-// TODO(kevinb) create a fraction layout
+
+const vsize = (node: Node) => {
+    switch (node.type) {
+        case 'Char': return charHeight(node) + charDepth(node)
+        case 'Box': return node.height + node.depth
+        case 'Rule': return node.height + node.depth
+        case 'Glue': return node.size
+        case 'Kern': return node.amount
+        default: return 0
+    }
+}
+
+const sum = (values: number[]): number => {
+    return values.reduce((res, val) => res + val, 0)
+}
+
+const vlistSize = (vlist: VList) => sum(vlist.map(vsize))
+
+const makeVBox = (node: Node, upList: VList, dnList: VList, shift = 0): VBox => {
+    return {
+        type: 'Box',
+        kind: 'VBox',
+        content: [...upList, node, ...dnList],
+        height: height(node) + vlistSize(upList),
+        depth: depth(node) + vlistSize(dnList),
+        shift: shift,
+    }
+}
+
+const numerator = makeHBox([
+    sansSerifChar('1')
+])
+
+const denominator = makeHBox([
+    sansSerifChar('2'),
+    makeKern(0.2),
+    sansSerifChar('+'),
+    makeKern(0.2),
+    sansSerifChar('3'),
+])
+
+const makeRule = (height: number, depth: number): Rule => {
+    return {
+        type: 'Rule',
+        height: height,
+        depth: depth,
+    }
+}
+
+const makeGlue = (size: number, shrink = 0, stretch = 0): Glue => {
+    return {
+        type: 'Glue',
+        size: size,
+        shrink: shrink,
+        stretch: stretch,
+    }
+}
+
+const fraction = makeVBox(
+    makeRule(0.1, 0.1),  // reference node
+    [
+        makeHBox([makeGlue(Infinity), numerator, makeGlue(Infinity)]),
+    ],  // upList
+    [
+        makeHBox([makeGlue(Infinity), denominator, makeGlue(Infinity)]),
+    ],  // dnList
+)
+
+console.log(fraction);
